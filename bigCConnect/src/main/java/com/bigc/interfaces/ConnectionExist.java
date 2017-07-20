0db37700ee42e4966148d66b6c3af029b13927f2
pragati <pragati@beesolvertechnology.com>
2017-07-20 16:40:58 +0530

    Connections Interface

---
+++ bigCConnect/src/main/java/com/bigc/interfaces/ConnectionExist.java
@@ -0,0 +1,13 @@
+package com.bigc.interfaces;
+
+import com.bigc.models.Users;
+
+/**
+ * Created by beesolver on 7/20/2017.
+ */
+
+public interface ConnectionExist {
+
+        public  void isConnection(boolean isConnection, Users user);
+
+}
